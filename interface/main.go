8987609aa30b0f14aa7fce8448b6802551261928
hezefeng <hezefeng@cf.cn>
2022-06-13 20:05:25 +0800

    add example

--- interface/main.go
+++ interface/main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"fmt"
+)
+
+type Phone interface {
+	call()
+}
+
+type NokiaPhone struct {
+}
+
+func (nokiaPhone NokiaPhone) call() {
+	fmt.Println("I am Nokia ,i can call you ")
+}
+
+type IPhone struct {
+}
+
+func (iphone IPhone) call() {
+	fmt.Println("I am iphone,i can call you either")
+}
+
 func main() {
+	var phone Phone
+	phone = new(NokiaPhone)
+	phone.call()
+
+	phone = new(IPhone)
+	phone.call()
 
 }
