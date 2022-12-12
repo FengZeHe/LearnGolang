96b2d53a880e470ee0a9f99cb10bf23ab78abe19
hezefeng <hzf1998@126.com>
2022-12-12 14:35:29 +0800

    update README.md

--- basis/context/main.go
+++ basis/context/main.go
@@ -6,26 +6,65 @@ import (
 	"time"
 )
 
+/*
+context 是 goroutine 的上下文，包含 goroutine 的运行状态、环境、现场等信息。
+context 主要用来在 goroutine 之间传递上下文信息，包括：取消信号、超时时间、截止时间、k-v 等。
+*/
 func main() {
-	baseCtx := context.Background()
-	ctx := context.WithValue(baseCtx, "name", "feng")
-	ctxcancel, cancel := context.WithCancel(baseCtx)
-	fmt.Println(ctx.Value("name"))
+	//WithTimeout()
+	//WithCancel()
+	//WithDeadline()
+	WithValue()
+}
+
+func WithTimeout() {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*2)
+	defer cancel()
+
+	start := time.Now().Unix()
+	<-ctx.Done()
+	end := time.Now().Unix()
+	//输出2说明这里阻塞的两秒
+	fmt.Println(end - start)
+}
+
+func WithCancel() {
+	ctx, cancel := context.WithCancel(context.Background())
 
 	go func() {
-		for {
-			select {
-			case <-ctxcancel.Done():
-				fmt.Println("done")
-				return
-			default:
-				fmt.Println("run")
-			}
-		}
+		<-ctx.Done()
+		fmt.Println("context was canceled")
 	}()
 
-	time.Sleep(1 * time.Second)
-	fmt.Println("Stop")
+	time.Sleep(time.Second)
 	cancel()
-	time.Sleep(1 * time.Second)
+	time.Sleep(time.Second)
+}
+
+func WithDeadline() {
+	ctx, cancel := context.WithDeadline(context.Background(), time.Now().Add(2*time.Second))
+	defer cancel()
+
+	start := time.Now().Unix()
+	<-ctx.Done()
+	end := time.Now().Unix()
+	fmt.Println(end - start)
+}
+
+func WithValue() {
+	parentKey := "parent"
+	parent := context.WithValue(context.Background(), parentKey, "this is parent")
+
+	sonKey := "son"
+	son := context.WithValue(context.Background(), sonKey, "this is son")
+
+	if parent.Value(parentKey) == nil {
+		fmt.Println("parent can not get son's key-value pair")
+	}
+
+	if val := son.Value(parentKey); val != nil {
+		fmt.Println("parent can not get son's  key-value pair")
+	}
+	fmt.Println(parent.Value(parentKey), son.Value(sonKey))
+
 }
--- basis/sync/atomic.go
+++ basis/sync/atomic.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"sync/atomic"
+)
+
 /*
 Go中的原子性：一个或多个操作在CPU的执行过程中不被中断的特性被称为原子性。这些操作对外表现成一个不可分割的整体，
 他们要么都执行，要么都不执行，外界不会看到它们执行到一半的状态。
@@ -9,5 +14,21 @@ Go中的原子性：一个或多个操作在CPU的执行过程中不被中断的
 var value int32 = 0
 
 func main() {
+	atomic.AddInt32(&value, 10)
+	nv := atomic.LoadInt32(&value)
+	fmt.Println(nv, value)
+	//	输出是10
 
+	swapped := atomic.CompareAndSwapInt32(&value, 10, 20)
+	fmt.Println(swapped, value)
+
+	swapped = atomic.CompareAndSwapInt32(&value, 20, 50)
+	fmt.Println(swapped, value)
+
+	swapped = atomic.CompareAndSwapInt32(&value, 19, 60)
+	fmt.Println(swapped, value)
+	//	因为旧值填的是19和之前的值不一样，所以交换值失败
+
+	old := atomic.SwapInt32(&value, 40)
+	fmt.Println(old, value)
 }
