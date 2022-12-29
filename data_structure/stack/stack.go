4f8c5a1fa897627d7fb05097e7ce940b8a0072ce
hezefeng <hzf1998@126.com>
2022-12-29 23:15:24 +0800

    update stack.go

--- data_structure/stack/stack.go
+++ data_structure/stack/stack.go
@@ -1,1 +1,43 @@
 package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Item interface {
+}
+
+type ItemStack struct {
+	items []Item
+	lock  sync.RWMutex
+}
+
+func NewStack() *ItemStack {
+	s := &ItemStack{}
+	s.items = []Item{}
+	return s
+}
+
+func (s *ItemStack) Print() {
+	fmt.Println(s.items)
+}
+
+func (s *ItemStack) Push(t Item) {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+
+	s.items = append(s.items, t)
+}
+
+func (s *ItemStack) Pop() Item {
+	s.lock.Lock()
+	defer s.lock.Unlock()
+	if len(s.items) == 0 {
+		return nil
+	}
+
+	item := s.items[len(s.items)-1]
+	s.items = s.items[0 : len(s.items)-1]
+	return item
+}
