fdc38fceeca11746efb50a7adfb847a5113a7b34
hezefeng <hezefeng@cf.cn>
2023-11-06 16:33:52 +0800

    update

--- project/BasicProject/dao/mysql/mysql.go
+++ project/BasicProject/dao/mysql/mysql.go
@@ -10,7 +10,7 @@ import (
 var db *gorm.DB
 
 func Init(cfg *setting.Mysql) (err error) {
-	dsn := fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=Local", cfg.User, cfg.Password, cfg.DB)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local", cfg.User, cfg.Password, cfg.Host, cfg.Port, cfg.DB)
 	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
