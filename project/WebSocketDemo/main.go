0eec2212b10d5ca85060f73d1cce61c79b831409
feng <hzf1998@126.com>
2024-02-13 10:26:17 +0800

    update

--- project/WebSocketDemo/main.go
+++ project/WebSocketDemo/main.go
@@ -5,6 +5,8 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
 	Cors "github.com/websocketdemo/Middleware/cors"
+	"log"
+	"net/http"
 )
 
 var upgrader = websocket.Upgrader{
@@ -12,9 +14,19 @@ var upgrader = websocket.Upgrader{
 	WriteBufferSize: 1024,
 }
 
+// var UpGrader= websocket.Upgrader{CheckOrigin: func (r *http.Request) bool {return true}
+var UpGrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
 func WebSocketHandler(c *gin.Context) {
-	// 获取WebSocket连接
-	ws, err := websocket.Upgrade(c.Writer, c.Request, nil, 1024, 1024)
+	// 获取WebSocket连接 下面这行代码使用的方法已经被弃用
+	//ws, err := websocket.Upgrade(c.Writer, c.Request, nil, 1024, 1024)
+	ws, err := UpGrader.Upgrade(c.Writer, c.Request, nil)
 	if err != nil {
 		panic(err)
 	}
@@ -26,15 +38,20 @@ func WebSocketHandler(c *gin.Context) {
 			break
 		}
 
-		fmt.Println("messageType:", messageType)
-		fmt.Println("p:", string(p))
+		fmt.Println("messageType:--?", messageType)
+		fmt.Println("msg--:", string(p))
 
 		// 输出WebSocket消息内容
-		c.Writer.Write(p)
+		//c.Writer.Write(p)
+
+		err = ws.WriteMessage(messageType, []byte("copy"))
+		if err != nil {
+			log.Println("websocket write Message ERROR")
+		}
 	}
 
 	// 关闭WebSocket连接
-	ws.Close()
+	defer ws.Close()
 }
 
 func main() {
