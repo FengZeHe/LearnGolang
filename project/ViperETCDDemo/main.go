96f5c34f5905334ca56e88b12069c257461573ff
hezefeng <hzf1998@126.com>
2024-07-03 22:08:26 +0800

    update

--- project/ViperETCDDemo/main.go
+++ project/ViperETCDDemo/main.go
@@ -20,21 +20,44 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println("connect to etcd success")
-	//defer cli.Close()
+	defer cli.Close()
 
-	get, err := cli.Get(context.Background(), "/config/mysql")
-	if err != nil {
-		return
+	if err = viper.AddRemoteProvider("etcd3", "http://127.0.0.1:2379", "/config/mysql"); err != nil {
+		log.Fatal("Viper Add Remote Provider ERROR", err)
 	}
-	fmt.Println("get=", get)
+	viper.SetConfigType("yaml")
 
-	v := viper.New()
-	v.SetConfigType("yaml")
-	v.AddRemoteProvider("etcd3", "https://127.0.0.1:2379", "/config/mysql")
-
-	if err := v.ReadRemoteConfig(); err != nil {
+	if err := viper.ReadRemoteConfig(); err != nil {
 		log.Fatal("ReadRemoteConfig", err)
 	}
-	value := v.GetStringMap("mysql")
+	value := viper.GetStringMap("mysql")
 	log.Println(value)
+
+	// 监听etcd变化
+	go watchEtcdChanges(cli)
+
+	// 阻塞主线程，使程序保持运行
+	select {}
+
+}
+
+// watchEtcdChanges 监听etcd中配置变化，更新viper的配置
+func watchEtcdChanges(etcdclient *clientv3.Client) {
+	watcher := clientv3.NewWatcher(etcdclient)
+
+	// 设定etcd中监听的路径
+	watchChan := watcher.Watch(context.Background(), "/config/mysql")
+	for resp := range watchChan {
+		for _, event := range resp.Events {
+			if event.Type == clientv3.EventTypePut {
+				// 更新Viper中的配置
+				if err := viper.ReadRemoteConfig(); err != nil {
+					log.Println("Viper Add Remote Provider ERROR", err)
+					continue
+				}
+				value := viper.GetStringMap("mysql")
+				fmt.Println("监听到值变化, value = ", value)
+			}
+		}
+	}
 }
