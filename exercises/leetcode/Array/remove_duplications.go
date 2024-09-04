5c616a5390109640808bc403279a160b641b6d3a
hezefeng <hezefeng@cf.cn>
2024-09-04 08:35:18 +0800

    xx

--- exercises/leetcode/Array/remove_duplications.go
+++ exercises/leetcode/Array/remove_duplications.go
@@ -1,11 +1,13 @@
 package main
 
+import "log"
+
 /*
 给你一个 升序排列 的数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次，返回删除后数组的新长度。元素的 相对顺序 应该保持一致 。
 
-由于在某些语言中不能改变数组的长度，所以必须将结果放在数组nums的第一部分。更规范地说，如果在删除重复项之后有 k 个元素，那么 nums 的前 k 个元素应该保存最终结果。
+由于在某些语言中不能改变数组的长度，所以必须将结果放在数组nums的第一部分。更规范地说，如果在删除重复项之后有 k 个元素，那么nums的前 k 个元素应该保存最终结果。
 
-将最终结果插入 nums 的前 k 个位置后返回 k 。
+将最终结果插入nums 的前 k 个位置后返回 k 。
 
 不要使用额外的空间，你必须在 原地 修改输入数组 并在使用 O(1) 额外空间的条件下完成。
 
@@ -43,6 +45,31 @@ for (int i = 0; i < k; i++) {
 nums 已按 升序 排列
 */
 
+func main() {
+	var nums []int = []int{1, 1, 1}
+	res := removeDuplicates(nums)
+	log.Println(res)
+}
+
+/*
+思路：
+1. 如果自己是第一个，检查自己后面是否与自己一致，如果一直则直接删掉
+
+将这些数组切割出来，循环分别取数组的第一个值
+
+	但是数组的下标会改变，改变后
+*/
 func removeDuplicates(nums []int) int {
-	return 0
+
+	for i := 0; i < len(nums); i++ {
+		if i+1 < len(nums) && nums[i] == nums[i+1] {
+			nums = append(nums[:i], nums[i+1:]...)
+			i = 0
+		}
+
+		log.Println("nums", nums)
+	}
+	k := len(nums)
+
+	return k
 }
