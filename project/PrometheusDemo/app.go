0afae49d49b485fe31b9a89e0563650af1422dee
hezefeng <hzf1998@126.com>
2025-03-02 18:52:43 +0800

    Changes

--- project/PrometheusDemo/app.go
+++ project/PrometheusDemo/app.go
@@ -1,7 +1,25 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/robfig/cron/v3"
+)
 
 type App struct {
 	server *gin.Engine
+	cron   *cron.Cron
 }
+
+// 定义一个计数器
+//var httpRequestsTotal = prometheus.NewCounterVec(
+//	prometheus.CounterOpts{
+//		Name: "http_requests_total",
+//		Help: "Total number of HTTP requests.",
+//	},
+//	[]string{"path", "method"},
+//)
+
+//func init() {
+//	// 注册计数器
+//	prometheus.MustRegister(httpRequestsTotal)
+//}
--- project/PrometheusDemo/main.go
+++ project/PrometheusDemo/main.go
@@ -1,12 +1,32 @@
 package main
 
-func main() {
+import (
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"net/http"
+)
 
+func main() {
 	app := InitializeApp()
+
+	initPrometheus()
+
+	app.cron.Start()
+	defer func() {
+		// 等待定时任务退出
+		<-app.cron.Stop().Done()
+	}()
+
 	server := app.server
 	err := server.Run(":8088")
 	if err != nil {
 		return
 	}
+}
 
+func initPrometheus() {
+	go func() {
+		// 专门给 prometheus 用的端口
+		http.Handle("/metrics", promhttp.Handler())
+		http.ListenAndServe(":8081", nil)
+	}()
 }
