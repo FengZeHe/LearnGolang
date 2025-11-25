b9ed2ea4bf59ce3fd3094ef73a3e5ed51fa7e48e
hezefeng <hzf1998@126.com>
2025-11-25 10:51:18 +0800

    finish like

--- project/BasicProjectV2/interactive/repository/dao/interactive.go
+++ project/BasicProjectV2/interactive/repository/dao/interactive.go
@@ -42,37 +42,32 @@ func (i *GORMInteractive) AddReadCount(aid string, ctx context.Context) (err err
 }
 
 func (i *GORMInteractive) HandleLike(aid string, like int, uid string, ctx context.Context) (err error) {
-	var likeArg int
-	if like == 0 {
-		likeArg = -1
-	} else {
-		likeArg = 1
-	}
-	/*
-		todo 1. 点赞数+1
-			 2. 加一条用户点赞文章的记录
-	*/
 	now := time.Now().Format("2006-01-02 15:04:05")
 	return i.db.Transaction(func(tx *gorm.DB) error {
-		// todo 第一段操作
-		if err = i.db.Model(domain.Interactive{}).Table("interactive").
-			Clauses(clause.OnConflict{
-				DoUpdates: clause.Assignments(map[string]any{
-					"like_count": gorm.Expr("like_count + ?", likeArg),
-					"utime":      now,
-				}),
-			}).Create(&domain.Interactive{
-			Aid:       aid,
-			LikeCount: 1,
-			CTime:     now,
-			UTime:     now,
-		}).Error; err != nil {
-			log.Println("interactive like", err)
-			return err
+		// 前置操作：先查询用户之前是否有点赞记录
+		var likeRec domain.LikeRecord
+		err = tx.Model(domain.LikeRecord{}).Table("like_record").Where("aid = ? AND uid = ?", aid, uid).First(&likeRec).Error
+		//lickExists := !errors.Is(err, gorm.ErrRecordNotFound)
+
+		switch {
+		// 之前没点赞,现在点赞
+		case like == 1 && likeRec.Like == 0:
+			if err = tx.Model(domain.Interactive{}).Table("interactive").Where("aid = ?", aid).
+				UpdateColumn("like_count", gorm.Expr("like_count + ?", 1)).Error; err != nil {
+				log.Println("update interactive like count error", err)
+				return err
+			}
+		// 之前点赞，现在取消点赞
+		case like == 0 && likeRec.Like == 1:
+			if err = tx.Model(domain.Interactive{}).Table("interactive").Where("aid = ?", aid).
+				UpdateColumn("like_count", gorm.Expr("like_count + ?", -1)).Error; err != nil {
+			}
+		default:
+			// 无操作
 		}
 
-		// ToDo 第二段操作
-		if err = i.db.Model(domain.LikeRecord{}).Table("like_record").Clauses(clause.OnConflict{
+		// 第二段操作 添加/更新like记录表
+		if err = tx.Model(domain.LikeRecord{}).Table("like_record").Clauses(clause.OnConflict{
 			Columns: []clause.Column{{Name: "aid"}, {Name: "uid"}},
 			DoUpdates: clause.Assignments(map[string]any{
 				"like":  like,
