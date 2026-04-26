bcc2e8dbfdf83edbb06e71a9da2686a22c867144
feng <hzf1998@126.com>
2026-04-26 17:37:18 +0800

    update

--- project/BasicProjectV2/relationship/domain/relationship.go
+++ project/BasicProjectV2/relationship/domain/relationship.go
@@ -17,3 +17,31 @@ type UserFollow struct {
 func (UserFollow) TableName() string {
 	return "user_follow"
 }
+
+type User struct {
+	ID       string `json:"id"`
+	Email    string `json:"email"`
+	Password string `json:"-"`
+	Phone    string `json:"phone"`
+	Birthday int    `json:"birthday"`
+	Nickname string `json:"nickname"`
+	Aboutme  string `json:"aboutme"`
+	Role     string `json:"role"`
+}
+
+func (User) TableName() string {
+	return "users"
+}
+
+type Relationship struct {
+	ID          string `gorm:"primaryKey;autoIncrement;column:id" json:"id"`
+	Uid         string `gorm:"column:uid;" json:"uid"`
+	FolloweeNum int64  `gorm:"column:followee_num;CHECK(followee_num >= 0)" json:"followee_num"`
+	FollowerNum int64  `gorm:"column:follower_num;CHECK(follower_num >= 0)" json:"follower_num"`
+	CreatedAt   string `gorm:"column:created_at" json:"created_at"`
+	UpdatedAt   string `gorm:"column:updated_at" json:"updated_at"`
+}
+
+func (Relationship) TableName() string {
+	return "relationship_record"
+}
--- project/BasicProjectV2/relationship/repository/dao/relationship.go
+++ project/BasicProjectV2/relationship/repository/dao/relationship.go
@@ -2,6 +2,7 @@ package dao
 
 import (
 	"context"
+	"log"
 	"strconv"
 	"time"
 
@@ -29,19 +30,19 @@ func NewGORMRelationshipDAO(db *gorm.DB) RelationshipDAO {
 }
 
 func (d *GORMRelationship) HandleFollow(followeeUId, followerUId string, action int, ctx context.Context) (err error) {
+
 	// todo  1. 修改follow关系 2. 更新relationship_record表
-	//var operation int
-	//switch action {
-	//case "0":
-	//	operation = Invariant
-	//case "1":
-	//	operation = Increase
-	//default:
-	//	operation = Invariant
-	//}
 	now := time.Now().Format("2006-01-02 15:04:05")
 
 	return d.db.Transaction(func(tx *gorm.DB) error {
+		followTarget := domain.User{}
+		if err = tx.Model(&domain.User{}).Where("id = ?", followerUId).First(&followTarget).Error; err != nil {
+			// 若没查到数据，会返回err "record not found"
+			return err
+		}
+		followStatus := domain.UserFollow{}
+		tx.Model(&domain.UserFollow{}).Where("followee_id = ? and follower_id = ?", followeeUId, followerUId).First(&followStatus)
+		beforeStatus := followStatus.Status
 
 		if err = tx.Model(&domain.UserFollow{}).Clauses(clause.OnConflict{
 			Columns: []clause.Column{{Name: "followee_id"}, {Name: "follower_id"}},
@@ -57,6 +58,75 @@ func (d *GORMRelationship) HandleFollow(followeeUId, followerUId string, action 
 			UpdatedAt:  now,
 		}).Error; err != nil {
 			return err
+		}
+
+		// todo 如果之前已关注 followee 就不增加  (relationship_record)
+		switch {
+		case beforeStatus == strconv.Itoa(action):
+			log.Println("已有记录，不增加了")
+			return nil
+		case beforeStatus != strconv.Itoa(action) && action == 1:
+			if err = tx.Model(&domain.Relationship{}).Clauses(clause.OnConflict{
+				Columns: []clause.Column{{Name: "uid"}},
+				DoUpdates: clause.Assignments(map[string]any{
+					"followee_num": gorm.Expr("followee_num + ?", Increase),
+				}),
+			}).Create(&domain.Relationship{
+				Uid:         followeeUId,
+				FolloweeNum: int64(Increase),
+				FollowerNum: 0,
+				CreatedAt:   now,
+			}).Error; err != nil {
+				return err
+			}
+
+			if err = tx.Model(&domain.Relationship{}).Clauses(clause.OnConflict{
+				Columns: []clause.Column{{Name: "uid"}},
+				DoUpdates: clause.Assignments(map[string]any{
+					"follower_num": gorm.Expr("follower_num + ?", Increase),
+				}),
+			}).Create(&domain.Relationship{
+				Uid:         followerUId,
+				FolloweeNum: 0,
+				FollowerNum: int64(Increase),
+				CreatedAt:   now,
+			}).Error; err != nil {
+				return err
+			}
+
+			log.Println("followee 关注+1 , follower 粉丝+1")
+
+		case beforeStatus != strconv.Itoa(action) && action == 0:
+			if err = tx.Model(&domain.Relationship{}).Clauses(clause.OnConflict{
+				Columns: []clause.Column{{Name: "uid"}},
+				DoUpdates: clause.Assignments(map[string]any{
+					"followee_num": gorm.Expr("followee_num + ?", Reduce),
+				}),
+			}).Create(&domain.Relationship{
+				Uid:         followeeUId,
+				FolloweeNum: int64(Reduce),
+				FollowerNum: 0,
+				CreatedAt:   now,
+			}).Error; err != nil {
+				return err
+			}
+
+			if err = tx.Model(&domain.Relationship{}).Clauses(clause.OnConflict{
+				Columns: []clause.Column{{Name: "uid"}},
+				DoUpdates: clause.Assignments(map[string]any{
+					"follower_num": gorm.Expr("follower_num + ?", Reduce),
+				}),
+			}).Create(&domain.Relationship{
+				Uid:         followerUId,
+				FolloweeNum: 0,
+				FollowerNum: int64(Reduce),
+				CreatedAt:   now,
+			}).Error; err != nil {
+				return err
+			}
+
+			log.Println("followee 关注-1 , follower 粉丝-1")
+
 		}
 
 		return nil
